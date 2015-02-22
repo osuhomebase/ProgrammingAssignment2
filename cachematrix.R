4037f8155856c144d421268bfa3355d9a49b2d6a
Mike Baron <mbaron999@gmail.com>
2015-02-21 21:03:48 -0500

    cacheSolve completed with comments

--- cachematrix.R
+++ cachematrix.R
@@ -4,30 +4,48 @@
 ## Create Matrix object that stores inverse
 
 makeCacheMatrix <- function(neo = matrix()) {
-    # store cached inverse matrix
+    ## store cached inverse matrix
     inverse <- NULL
     
-    # set matrix
+    ## set matrix
     set <- function(x) {
         neo <<- x;
         inverse <<- NULL;
     }
     
-    # get matrix
+    ## get matrix
     get <- function() return(neo);
     
-    # set inverse
+    ## set inverse
     setinv <- function(inv) inverse <<- inv;
-    # get inverse
+    ## get inverse
     getinv <- function() return(inverse);
     
-    # return everything with new values
+    ## return everything with new values
     return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the matrix, unless it's already figured out, then just return it
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    ## set the inverse
+    inv <- x$getinv()
+
+    ## If we already know the inverse, we're done here
+    if (!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+
+    ## figure out inverse
+    data <- x$get()
+    inv <- solve(data, ...)
+
+    ## Cache inverse
+    x$setinv(inv)
+    
+    ## remember the last thing done is what is returned
+    inv
 }
