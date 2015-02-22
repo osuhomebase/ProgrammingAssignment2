6f487e75b54c26b22cf96ab8f98530f380cdb439
Mike Baron <mbaron999@gmail.com>
2015-02-21 20:58:35 -0500

    updated makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,26 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(neo = matrix()) {
+    # store cached inverse matrix
+    inverse <- NULL
+    
+    # set matrix
+    set <- function(x) {
+        neo <<- x;
+        inverse <<- NULL;
+    }
+    
+    # get matrix
+    get <- function() return(neo);
+    
+    # set inverse
+    setinv <- function(inv) inverse <<- inv;
+    # get inverse
+    getinv <- function() return(inverse);
+    
+    # return everything with new values
+    return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
 
 
